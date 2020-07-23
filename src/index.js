706b3d5e756e88047c5bef4224fe3afd11148645
Andy Thompson <athompson@inviqa.com>
2020-07-23 09:48:46 +0100

    Move output to github pages subdirectory

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const App = require('./app')
 
 async function run() {
     const schemas = await App.readCrdSchemasFromDirectory('manifests')
-    await App.writeSchemasTo('build/master-standalone', schemas)
+    await App.writeSchemasTo('docs/schema/master-standalone', schemas)
 }
 
 run()
