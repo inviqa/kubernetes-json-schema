6a05fd5a8b5b282fe1ebe6d17f28fcb7c7f2748c
Andy Thompson <athompson@inviqa.com>
2020-09-10 09:51:40 +0100

    Add 1.17 for EKS validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const App = require('./app')
 async function run() {
     const schemas = await App.readCrdSchemasFromDirectory('manifests')
     await App.writeSchemasTo('docs/schema/master-standalone', schemas)
+    // though there are later releases of 1.17, kubeval schemas only supplies up to 1.17.4
+    await App.writeSchemasTo('docs/schema/1.17.4-standalone', schemas)
 }
 
 run()
