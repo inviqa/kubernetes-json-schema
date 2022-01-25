072372d9bf7a23583da50a4e795364b0ae989f62
Andy Thompson <athompson@inviqa.com>
2022-01-25 10:42:49 +0000

    Add additional schema directories for the most recent specific k8s bugfix versions

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ const App = require('./app')
 
 async function run() {
     const schemas = await App.readCrdSchemasFromDirectory('manifests')
+    // future additional versions of supported k8s releases should be added
     await App.writeSchemasTo('docs/schema/master-standalone', schemas)
-    // though there are later releases of 1.17, kubeval schemas only supplies up to 1.17.4
     await App.writeSchemasTo('docs/schema/1.17.4-standalone', schemas)
+    await App.writeSchemasTo('docs/schema/v1.19.16-standalone', schemas)
+    await App.writeSchemasTo('docs/schema/v1.20.15-standalone', schemas)
+    await App.writeSchemasTo('docs/schema/v1.21.9-standalone', schemas)
+    await App.writeSchemasTo('docs/schema/v1.22.6-standalone', schemas)
+    await App.writeSchemasTo('docs/schema/v1.23.2-standalone', schemas)
 }
 
 run()
